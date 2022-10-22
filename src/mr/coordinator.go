5be5322c9c39adee73271dd1148c3d9f0213fcda
shency <alex_shency@outlook.com>
2022-10-23 02:08:08 +0800

    重启挂了的任务

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,6 +1,7 @@
 package mr
 
 import (
+	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -40,7 +41,7 @@ type Task struct {
 const (
 	Map       = 0
 	Reduce    = 1
-	Idle      = 2
+	Wait      = 2
 	Completed = 3
 )
 
@@ -58,7 +59,86 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 
 // GiveTask : 返回reply，告知worker任务信息
 func (c *Coordinator) GiveTask(args *RPCArgs, reply *RPCReply) error {
-	c.
+	// 操作队列的时候要加锁
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	// 重启挂了的任务（超过10s）
+	curTime := time.Now().Unix()
+	for i, task := range c.mapTasksInProgress {
+		if curTime-task.TimeStamp > 10 {
+			c.mapTasksReady[i] = task
+			delete(c.mapTasksInProgress, i)
+			fmt.Printf("重启Map任务 %d", i)
+
+		}
+	}
+	for i, task := range c.reduceTasksInProgress {
+		if curTime-task.TimeStamp > 10 {
+			c.reduceTasksReady[i] = task
+			delete(c.reduceTasksInProgress, i)
+			fmt.Printf("重启Reduce任务 %d", i)
+
+		}
+	}
+
+	// 处于map阶段
+	if len(c.mapTasksReady) > 0 {
+		// 因为map遍历是随机的，因此随机取一个任务
+		for i, task := range c.mapTasksReady {
+			// 设置当前task的时间戳
+			task.TimeStamp = time.Now().Unix()
+			// 将这个任务发给reply
+			reply.TaskInfo = task
+			// 将这个任务转移到inProgress队列
+			c.mapTasksInProgress[i] = task
+			delete(c.mapTasksReady, i)
+			fmt.Printf("发送Map任务给Worker", reply.TaskInfo)
+			return nil
+		}
+	} else if len(c.mapTasksInProgress) > 0 {
+		// 仍然有map任务在执行，则等待
+		reply.TaskInfo = Task{TaskType: Wait}
+		return nil
+	}
+
+	// 检测是否完成map阶段
+	// 在进入reduce阶段前，要生成所有的Reduce Task
+	if !c.reduceReady {
+		for i := 0; i < c.NReduce; i++ {
+			c.reduceTasksReady[i] = Task{
+				TaskType:  Reduce,
+				TaskID:    i,
+				NReduce:   c.NReduce,
+				MMap:      c.MMap,
+				TimeStamp: time.Now().Unix()}
+		}
+		c.reduceReady = true
+	}
+
+	// 处于reduce阶段
+	if len(c.reduceTasksReady) > 0 {
+		// 因为map遍历是随机的，因此随机取一个任务
+		for i, task := range c.reduceTasksReady {
+			// 设置当前task的时间戳
+			task.TimeStamp = time.Now().Unix()
+			// 将这个任务发给reply
+			reply.TaskInfo = task
+			// 将这个任务转移到inProgress队列
+			c.reduceTasksInProgress[i] = task
+			delete(c.reduceTasksReady, i)
+			fmt.Printf("发送Reduce任务给Worker", reply.TaskInfo)
+			return nil
+		}
+	} else if len(c.reduceTasksInProgress) > 0 {
+		// 仍然有reduce任务在执行，则等待
+		reply.TaskInfo = Task{TaskType: Wait}
+		return nil
+	} else {
+		// 完成了所有任务
+		reply.TaskInfo = Task{TaskType: Completed}
+	}
+
 	return nil
 }
 
